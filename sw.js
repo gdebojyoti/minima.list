51a08076f5251c5e1fb557f125846461fbfc2885
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2018-02-13 02:15:51 +0530

    Caching enabled via SW.

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
-self.addEventListener("install", () => console.log("installed"));
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    "index.html",
+    "public/js/bundle.js"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log("opened!");
+                return cache.addAll(urlsToCache);
+            })
+    );
+    console.log("installed");
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(e.request);
+            })
+    );
+    console.log("fetched");
+});
